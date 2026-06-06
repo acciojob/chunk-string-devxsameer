aa53fab1ed2df1ac8ea248c19df101611eeed15d
Sameer Ali <140313541+devxsameer@users.noreply.github.com>
2026-06-06 09:48:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-function chunkString(str, size) {
-  const result = [];
-
-  for (let i = 0; i < str.length; i += Number(size)) {
-    result.push(str.slice(i, i + Number(size)));
-  }
-
-  return result;
+function stringChop(str, size) {
+  // your code here
+	if(str===null){
+		return [];
+	}
+	let result=[];
+	for(let i=0;i<str.length;i+=size){
+		result.push(str.slice(i,i+size));
+	}
+	return result;
 }
 
 // Do not change the code below
 const str = prompt("Enter String.");
 const size = prompt("Enter Chunk Size.");
-alert(JSON.stringify(chunkString(str, size)));
+alert(stringChop(str, size));
